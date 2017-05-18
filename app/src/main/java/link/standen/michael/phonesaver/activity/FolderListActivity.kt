fba7f8e96488bd88fe5620f7cea6da1cb0ef092d
Michael Standen <screaminghawk@gmail.com>
2017-05-19 09:24:37 +1200

    Update Kotlin uses tabs

--- app/src/main/java/link/standen/michael/phonesaver/activity/FolderListActivity.kt
+++ app/src/main/java/link/standen/michael/phonesaver/activity/FolderListActivity.kt
@@ -2,35 +2,35 @@ package link.standen.michael.phonesaver.activity
 
 class FolderListActivity : android.support.v7.app.AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: android.os.Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(link.standen.michael.phonesaver.R.layout.folder_list_activity)
-        val toolbar = findViewById(link.standen.michael.phonesaver.R.id.toolbar) as android.support.v7.widget.Toolbar
-        setSupportActionBar(toolbar)
+	override fun onCreate(savedInstanceState: android.os.Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(link.standen.michael.phonesaver.R.layout.folder_list_activity)
+		val toolbar = findViewById(link.standen.michael.phonesaver.R.id.toolbar) as android.support.v7.widget.Toolbar
+		setSupportActionBar(toolbar)
 
-        val fab = findViewById(link.standen.michael.phonesaver.R.id.fab) as android.support.design.widget.FloatingActionButton
-        fab.setOnClickListener { _ ->
-            val intent = android.content.Intent(this@FolderListActivity, FolderSelectActivity::class.java)
-            this@FolderListActivity.startActivity(intent)
-        }
-    }
+		val fab = findViewById(link.standen.michael.phonesaver.R.id.fab) as android.support.design.widget.FloatingActionButton
+		fab.setOnClickListener { _ ->
+			val intent = android.content.Intent(this@FolderListActivity, FolderSelectActivity::class.java)
+			this@FolderListActivity.startActivity(intent)
+		}
+	}
 
-    override fun onCreateOptionsMenu(menu: android.view.Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(link.standen.michael.phonesaver.R.menu.folder_list_menu, menu)
-        return true
-    }
+	override fun onCreateOptionsMenu(menu: android.view.Menu): Boolean {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		menuInflater.inflate(link.standen.michael.phonesaver.R.menu.folder_list_menu, menu)
+		return true
+	}
 
-    override fun onOptionsItemSelected(item: android.view.MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        val id = item.itemId
+	override fun onOptionsItemSelected(item: android.view.MenuItem): Boolean {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		val id = item.itemId
 
-        if (id == link.standen.michael.phonesaver.R.id.action_settings) {
-            return true
-        }
+		if (id == link.standen.michael.phonesaver.R.id.action_settings) {
+			return true
+		}
 
-        return super.onOptionsItemSelected(item)
-    }
+		return super.onOptionsItemSelected(item)
+	}
 }
--- app/src/main/java/link/standen/michael/phonesaver/activity/FolderSelectActivity.kt
+++ app/src/main/java/link/standen/michael/phonesaver/activity/FolderSelectActivity.kt
@@ -8,24 +8,24 @@ import link.standen.michael.phonesaver.R
 
 class FolderSelectActivity : AppCompatActivity() {
 
-    private var mTextMessage: TextView? = null
+	private var mTextMessage: TextView? = null
 
-    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
-        when (item.itemId) {
-            R.id.navigation_here -> {
-                mTextMessage!!.setText(R.string.navigation_here)
-                return@OnNavigationItemSelectedListener true
-            }
-        }
-        false
-    }
+	private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+		when (item.itemId) {
+			R.id.navigation_here -> {
+				mTextMessage!!.setText(R.string.navigation_here)
+				return@OnNavigationItemSelectedListener true
+			}
+		}
+		false
+	}
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.folder_select_activity)
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.folder_select_activity)
 
-        mTextMessage = findViewById(R.id.message) as TextView
-        val navigation = findViewById(R.id.navigation) as BottomNavigationView
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
-    }
+		mTextMessage = findViewById(R.id.message) as TextView
+		val navigation = findViewById(R.id.navigation) as BottomNavigationView
+		navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+	}
 }
